1c2e3c74963e430dfeda63744fa03043b4ffbb79
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-01-19 12:29:56 -0300

    Update Matrizes.r

--- Matrizes.r
+++ Matrizes.r
@@ -1,16 +1,42 @@
-vetor1 <- c(1,2,3,4,5,6,7,8,9)
+
+### Entrada de dados
+
+vetor1 <- c(1,2,3,4,5,6,7,8,9) 
 matriz1 <- matrix(vetor1, nrow = 3, ncol = 3, byrow = TRUE)
-matriz1
 
-vetor2 <- c(2,4,6,8,0,2,4,6,8)
+vetor2 <- c(2,4,6,8,10,2,4,6,8)
 matriz2 <- matrix(vetor2, nrow = 3, ncol = 3, byrow = TRUE)
-matriz2
 
-#### Multiplicação de matrizes
-matriz1 %*% matriz2
+### Calculo de Matrizes
 
-#### Matriz transposta
-t(matriz1)
+saida <- '\n1) Multiplicação de uma matriz por um escalar:\n'
+message(saida)
+matriz1*2
 
-#### Determinante
+saida <- '\n2) Soma de matrizes:\n'
+message(saida)
+matriz1 + matriz2
+
+saida <- '\n3) Subtração de matrizes:\n'
+message(saida)
+matriz1 - matriz2
+
+saida <- '\n4) Multiplicação de matrizes\n'
+message(saida)
+matriz1 * matriz2
+
+saida <- '\n5) Divisão de matrizes:\n'
+message(saida)
+matriz1 / matriz2
+
+saida <- '\n6) Multiplicação de matrizes:\n'
+message(saida)
+matriz1%*%matriz2
+
+saida <- '\n7) Determinante de uma matriz:\n'
+message(saida)
 det(matriz1)
+
+saida <- '\n8) Matriz transposta:\n'
+message(saida)
+t(matriz1)
