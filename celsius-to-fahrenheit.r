9b9420501783c6746af884e7eb8af9b0607256cb
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-02-24 14:18:46 -0300

    Create celsius-to-fahrenheit.r

---
+++ celsius-to-fahrenheit.r
@@ -0,0 +1,9 @@
+# Função que converte temperatura em graus Celsius para Fahrenheit
+
+is.converte <- function () {
+ C = 40 
+ F = 1.8 * C + 32 
+ paste("Temperatura em Fahrenheit:", F)
+}
+
+is.converte ()
